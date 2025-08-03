c3b7d6ad759a4834f3259f7ffd79c7f0517cf1fe
Alex <chulgarev@gmail.com>
2025-08-03 21:14:09 +0700

    Refactoring

--- arrays/my_blinear_array.h
+++ arrays/my_blinear_array.h
@@ -5,12 +5,19 @@
 #include <vector>
 #include <algorithm>
 
+
 typedef uint32_t (*timer_cb)(void*);
 struct timer_data {
     uint32_t deadline;
     uint32_t id;
     void* userp;
     timer_cb callback;
+};
+
+struct timer
+{
+    uint32_t deadline;
+    uint32_t id;
 };
 
 
@@ -25,27 +32,33 @@ public:
 
     std::vector<struct timer_data> timeouts;
     uint32_t next_id = 0;
-    uint32_t prev{};
+    timer prev{};
 
     static bool is_after(const timer_data& lh, const timer_data& rh)
     {
         return lh.deadline >= rh.deadline;
     }
 
-    uint32_t schedule_timer(uint32_t deadline, timer_cb cb, void* userp) {
-        timer_data element{deadline, next_id++, userp, cb};
-        auto it = std::lower_bound(timeouts.begin(), timeouts.end(),
-                                   element, is_after);
-        // Insert new element
-        timeouts.insert(it, std::move(element));
-        return next_id;
+    timer schedule_timer(uint32_t deadline, timer_cb cb, void* userp)
+    {
+        timer_data element{deadline, next_id, userp, cb};
+        auto i = std::lower_bound(timeouts.begin(), timeouts.end(),
+                                  element, is_after);
+        timeouts.insert(i, element);
+        return {deadline, next_id++};
     }
 
-    void cancel_timer(uint32_t t)
+    void cancel_timer(timer t)
     {
-        auto i = std::find_if(timeouts.begin(), timeouts.end(),
-                              [t](const auto& e) { return e.id == t; });
-        timeouts.erase(i);
+        timer_data element{t.deadline, t.id, nullptr, nullptr};
+        auto [lo, hi] = std::equal_range(timeouts.begin(), timeouts.end(),
+                                         element, is_after);
+        auto i = std::find_if(lo, hi,
+                              [t](const auto& e) { return e.id == t.id; });
+        if (i != hi) {
+            std::move(i + 1, timeouts.end(), i);
+            timeouts.pop_back();
+        }
     }
 
     bool shoot_first()
@@ -58,7 +71,7 @@ public:
 
     void loop (uint32_t id, int i)
     {
-        uint32_t t = schedule_timer(id, [](void*){return 0U;}, nullptr);
+        timer t = schedule_timer(id, [](void*){return 0U;}, nullptr);
         if (i & 1) cancel_timer(prev);
         prev = t;
     }
